90a98be664d71218ea7f6a695e0ef8d70f889fbf
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2022-12-07 09:56:29 -0300

    Update execoes.ts

---
+++ execoes.ts
@@ -0,0 +1,17 @@
+class Duplicado extends Error{
+    constructor(message: string){
+        super(message);
+    }
+}
+
+class Vazio extends Error{
+    constructor(message: string){
+        super(message);
+    }
+}
+
+class Validacao extends Error{
+    constructor(message: string){
+        super(message);
+    }
+}
